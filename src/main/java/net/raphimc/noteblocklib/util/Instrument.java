394bbba49931d701a326b7274d42b484bebab515
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-11-15 21:55:15 +0100

    Added bedrock edition instrument ids

--- src/main/java/net/raphimc/noteblocklib/util/Instrument.java
+++ src/main/java/net/raphimc/noteblocklib/util/Instrument.java
@@ -19,30 +19,32 @@ package net.raphimc.noteblocklib.util;
 
 public enum Instrument {
 
-    HARP(0, 0),
-    BASS(1, 4),
-    BASS_DRUM(2, 1),
-    SNARE(3, 2),
-    HAT(4, 3),
-    GUITAR(5, 7),
-    FLUTE(6, 5),
-    BELL(7, 6),
-    CHIME(8, 8),
-    XYLOPHONE(9, 9),
-    IRON_XYLOPHONE(10, 10),
-    COW_BELL(11, 11),
-    DIDGERIDOO(12, 12),
-    BIT(13, 13),
-    BANJO(14, 14),
-    PLING(15, 15),
+    HARP(0, 0, 0),
+    BASS(1, 4, 4),
+    BASS_DRUM(2, 1, 1),
+    SNARE(3, 2, 2),
+    HAT(4, 3, 3),
+    GUITAR(5, 7, 8),
+    FLUTE(6, 5, 6),
+    BELL(7, 6, 5),
+    CHIME(8, 8, 7),
+    XYLOPHONE(9, 9, 9),
+    IRON_XYLOPHONE(10, 10, 10),
+    COW_BELL(11, 11, 11),
+    DIDGERIDOO(12, 12, 12),
+    BIT(13, 13, 13),
+    BANJO(14, 14, 14),
+    PLING(15, 15, 15),
     ;
 
     private final byte nbsId;
     private final byte mcId;
+    private final byte mcbeId;
 
-    Instrument(final int nbsId, final int mcId) {
+    Instrument(final int nbsId, final int mcId, final int mcbeId) {
         this.nbsId = (byte) nbsId;
         this.mcId = (byte) mcId;
+        this.mcbeId = (byte) mcbeId;
     }
 
     public byte nbsId() {
@@ -51,6 +53,10 @@ public enum Instrument {
 
     public byte mcId() {
         return this.mcId;
+    }
+
+    public byte mcbeId() {
+        return this.mcbeId;
     }
 
     public static Instrument fromNbsId(final byte nbsId) {
@@ -65,6 +71,15 @@ public enum Instrument {
     public static Instrument fromMcId(final byte mcId) {
         for (final Instrument instrument : Instrument.values()) {
             if (instrument.mcId == mcId) {
+                return instrument;
+            }
+        }
+        return null;
+    }
+
+    public static Instrument fromMcbeId(final byte mcbeId) {
+        for (final Instrument instrument : Instrument.values()) {
+            if (instrument.mcbeId == mcbeId) {
                 return instrument;
             }
         }
